b5958c627256488699f26c87018fa9008852411a
nickolaygrechkin@gmail.com <nickolaygrechkin@gmail.com>
2023-07-10 18:04:14 +0300

    Implement ProxySSGHelper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,12 +1,9 @@
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/api";
-import type { User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
-const filterUserForClient = (user: User) => {
-  return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
-}
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
